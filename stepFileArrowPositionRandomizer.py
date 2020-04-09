5f0fe90a8b5bfe737efc6d2d965e4e92528cb545
Mael Perreault <perreault.mael@gmail.com>
2020-04-09 03:01:30 -0400

    Updated help

--- stepFileArrowPositionRandomizer.py
+++ stepFileArrowPositionRandomizer.py
@@ -1,15 +1,27 @@
 # Stepfile Arrow Randomizer v.0.1 by Ombar
 #
-# Randomizes arrow positions in (.sm) files
-# All (.sm) files in program directory and sub-directorries are randomized
-# Backups of (.sm) files are created in case something goes terribly wrong
-# Randomization is really simple for now and simply prevents too many of the same arrows in a row
-# Basically just moves existing arrows around, you need to create the arrows first
-#
+# Randomizes arrow positions in (.sm) files for all (.sm) files in program directory and sub-directorries
+# Backups (.bak) of (.sm) files are created in case something goes wrong
+# Randomization is simple for now and simply prevents too many of the same arrows in a row
+# Only moves existing arrows around, you need to create the arrows first
+
 # HOW TO USE
-# Use ArrowVortex to create basic (.sm) file)
-# Add steps and hold arrows following the beat of the song
-# Run Randomizer in the same directory as the file to randomize arrow positions
+# 1. Use ArrowVortex to create basic (.sm) file
+# 2. Add steps and hold arrows following the beat of the song
+# 3. Run the randomizer in the same directory as the file to randomize arrow positions
+# 4. Copy the randomized ( your Stepmania Songs folder as usual
+
+# NOTES You will need to reopen file in ArrowVortex to see the changes
+# You might need to force a reload of the file in Stepmania to see the changes
+# Press CTRL+SHIFT+R in selection screen while the song is selected
+
+# WARNING In case something goes wrong, you can always use one of the .bak files
+# Simply remove the .001.bak to have your .sm file once again
+
+# DO NOT RUN THIS INSIDE YOUR MAIN STEPMANIA SONGS FOLDER THIS WILL RANDOMIZE ALL YOUR SONGS UNDOING THIS IS NOT SUPPORTED
+# I suggest you work in a folder on your desktop just in case
+# I suggest you keep your original (.sm) ifle with beats to do quick beat edits until you are done
+# You can simply run the randomizer again to test new patterns
 
 import os
 import random
@@ -244,19 +256,31 @@ directoryPath = os.getcwd()
 
 print("************************************************************")
 print(" Stepfile Arrow Randomizer v.0.1 by Ombar")
-print("")
-print(" Randomizes arrow positions in (.sm) files")
-print(" All (.sm) files in program directory and sub-directorries are randomized")
-print(" Backups of (.sm) files are created in case something goes terribly wrong")
-print(" Randomization is really simple for now and simply prevents too many of the same arrows in a row")
-print(" Just moves existing arrows around, you need to create the arrows first")
-print(" Run a couple times and test until you get a pattern you like")
-print("")
-print(" HOW TO USE")
-print(" Use ArrowVortex to create basic (.sm) file")
-print(" Add steps and hold arrows following the beat of the song")
-print(" Run Randomizer in the same directory as the file to randomize arrow positions")
-print("")
+print("""
+Randomizes arrow positions in (.sm) files for all (.sm) files in program directory and sub-directorries
+Backups (.bak) of (.sm) files are created in case something goes wrong
+Randomization is simple for now and simply prevents too many of the same arrows in a row
+Only moves existing arrows around, you need to create the arrows first
+
+HOW TO USE
+1. Use ArrowVortex to create basic (.sm) file
+2. Add steps and hold arrows following the beat of the song
+3. Run the randomizer in the same directory as the file to randomize arrow positions
+4. Copy the randomized ( your Stepmania Songs folder as usual
+
+NOTES You will need to reopen file in ArrowVortex to see the changes
+You might need to force a reload of the file in Stepmania to see the changes
+Press CTRL+SHIFT+R in selection screen while the song is selected
+
+WARNING 
+In case something goes wrong, you can always use one of the .bak files
+Simply remove the .001.bak to have your .sm file once again
+
+DO NOT RUN THIS INSIDE YOUR MAIN STEPMANIA SONGS FOLDER THIS WILL RANDOMIZE ALL YOUR SONGS UNDOING THIS IS NOT SUPPORTED
+I suggest you work in a folder on your desktop just in case
+I suggest you keep your original (.sm) ifle with beats to do quick beat edits until you are done
+You can simply run the randomizer again to test new patterns
+""")
 print("************************************************************")
 print(" Randomizing (.sm) files in directory and sub-directories :")
 print("    {}".format(directoryPath))
